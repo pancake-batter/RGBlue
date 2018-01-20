60dc54fab0fda4cfa8dfdf6cce33d761001c1597
alperenssahin <33184993+alperenssahin@users.noreply.github.com>
2018-01-20 23:55:50 +0300

    Update RGBleu.c

--- RGBleu.c
+++ RGBleu.c
@@ -5,12 +5,12 @@ int LedRedPIN = 11;
 int LedGreenPIN = 10;
 int LedBleuPIN = 9;
 
-int command ;// Bluetooth üzerinde alınan seri veriyi tutar 
+int command ; //keep the data that given from bluetooth module
 int a = 0;
-int dizi[2];//bluetooth üzerinden alınan 2 li veriyi tutar.
+int duo[2]; //create array for keep the color key and color intensity
 
 void setup() {
-//pin tanımlama
+//pin declarition
 
 pinMode(LedRedPIN,OUTPUT);
 pinMode(LedGreenPIN,OUTPUT);
@@ -30,35 +30,31 @@ pinMode(LedBleuPIN,OUTPUT);
 }
 
 void loop() {
-  
 
-
-
-  // Android cihazdan  veri alındığında if komutu çalışır 
   if (mySerial.available()) {
-    a %= 2; //alınan sıralı veriyi(renk anahtarı ve renk şiddeti), her döngüde dizi değişkenine kaydeder.
+    a %= 2;
     command = mySerial.read();
     Serial.println(command);
-    dizi[a] = command; 
-    //command +=(char)mySerial.read();
+    duo[a] = command; 
     a++;
   }
 
-  for(int i=0 ; i<2; i++){ //değişen rengin anahtarını aramak için dizi değişkenin elemanlarını teker teker switch komutuna sokar.
-    switch(dizi[i]){//dizi değişkenini yorumlar, hangi rengin değiştirildiğini saptayıp rgb() fonksyonuna parametre vererek rgb ledlerin parlıklığını değiştirir. 
+  for(int i=0 ; i<2; i++){ 
+    //değişen rengin anahtarını aramak için duo değişkenin elemanlarını teker teker switch komutuna sokar.
+    //switch search color key
+    switch(duo[i]){
       case 0:
-      rgb(0,dizi[(i+1)%2]); // dizi değişkenşndekş anahtar yakalandığında anahtardan önceki yada sonraki elemanı rgb() fonksyonuna parlaklık parametresi olarak atar.
-      break;
+      rgb(0,duo[(i+1)%2]); // dizi değişkenşndekş anahtar yakalandığında anahtardan önceki yada sonraki elemanı rgb() fonksyonuna parlaklık parametresi olarak atar.
+      break;               //declare the key and other value of duo array to parametres of rgb(key,intensity) function 
       case 1:
-      rgb(1,dizi[(i+1)%2]);
+      rgb(1,duo[(i+1)%2]);
       break;
       case 2:
-      rgb(2,dizi[(i+1)%2]);
+      rgb(2,duo[(i+1)%2]);
       break;
     }
   }
   
-  // bilgisayarın seri monitörü üzerinden ardunio-cihaz haberleşmesi için gerekli kodlar.
   if (Serial.available()){
        //Serial.print("AT");
     delay(10); 
@@ -70,15 +66,15 @@ void loop() {
 }
 
 //girilen renk anahtarını saptayıp, ledin tonunu değiştirir.
-void rgb(byte renk, byte ton){
-if(renk == 0){
-  analogWrite(LedRedPIN,abs(ton-255));
+void rgb(byte key, byte intensity){
+if(key == 0){
+  analogWrite(LedRedPIN,abs(intensity-255));
 }
-if(renk == 1){
-  analogWrite(LedGreenPIN,abs(ton-255));
+if(key == 1){
+  analogWrite(LedGreenPIN,abs(intensity-255));
 }
-if(renk == 2){
-  analogWrite(LedBleuPIN,abs(ton-255));
+if(key == 2){
+  analogWrite(LedBleuPIN,abs(intensity-255));
 }
   
 }
